a820710b1d21a2ae296d6c94046d38b4d8bfa55c
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2021-02-20 22:28:18 +0530

    declate variables and create vector for location

--- app/src/main/java/com/insharp/android/mapbox/MainActivity.java
+++ app/src/main/java/com/insharp/android/mapbox/MainActivity.java
@@ -3,25 +3,63 @@ package com.insharp.android.mapbox;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.MapboxDirections;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.Layer;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation;
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
+    public MapboxMap mapboxMap;
+    private PermissionsManager permissionsManager;
+    LocationComponent locationComponent;
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+    private CarmenFeature home;
+    private CarmenFeature work;
+    private String geojsonSourceLayerId = "geojsonSourceLayerId";
+    private String symbolIconId = "symbolIconId";
+    private static final int REQUEST_CODE = 5678;
+    String address;
+    Point origin = Point.fromLngLat(90.399452, 23.777176);
+    Point destination = Point.fromLngLat(90.399452, 23.777176);
+    private static final String ROUTE_LAYER_ID = "route-layer-id";
+    private static final String ROUTE_SOURCE_ID = "route-source-id";
+    private static final String ICON_LAYER_ID = "icon-layer-id";
+    private static final String ICON_SOURCE_ID = "icon-source-id";
+    private static final String RED_PIN_ICON_ID = "red-pin-icon-id";
+    private MapboxDirections client;
+    int c = 0;
+    MapboxNavigation navigation;
+    double distance;
+    String st;
+    String startLocation = "";
+    String endLocation = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 //        Mapbox.getInstance(this, "pk.eyJ1IjoiZGlsem1hcGJveCIsImEiOiJja2w5YWp1ajMwbnZ6Mndydno0b2F3cm14In0.Ec9QDo9rPKV8pph8Ix3WEQ");
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
         setContentView(R.layout.activity_main);
+
+        navigation = new MapboxNavigation(this, getString(R.string.mapbox_access_token));
+
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
@@ -34,8 +72,11 @@ public class MainActivity extends AppCompatActivity {
                     public void onStyleLoaded(@NonNull Style style) {
 
                         // Map is set up and the style has loaded. Now you can add data or make other map adjustments
-
-
+//                        Layer waterLayer = style.getLayer("water");
+//
+//                        if (waterLayer != null) {
+//                            waterLayer.setProperties(PropertyFactory.fillColor(Color.parseColor("#004f6b")));
+//                        }
                     }
                 });
 
